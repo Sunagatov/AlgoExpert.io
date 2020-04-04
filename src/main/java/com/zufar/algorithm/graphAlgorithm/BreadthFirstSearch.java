b2d98da0fcb4fb03ad517d5f363e4374ddfb3d83
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2020-04-04 20:21:34 +0400

    BreadthFirstSearch algorithm was added.

---
+++ src/main/java/com/zufar/algorithm/graphAlgorithm/BreadthFirstSearch.java
@@ -0,0 +1,28 @@
+package com.zufar.algorithm.graphAlgorithm;
+
+import com.zufar.algorithm.structures.trees.TreeNode;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class BreadthFirstSearch {
+
+    public static TreeNode<Integer> findFirstByPredicate(TreeNode<Integer> root, Predicate<Integer> condition) {
+        ArrayDeque<TreeNode<Integer>> numbersQueue = new ArrayDeque<>(root.getChildren());
+        Set<TreeNode<Integer>> checkedNodes = new HashSet<>();
+        TreeNode<Integer> current;
+        do {
+            current = numbersQueue.pollFirst();
+            if (checkedNodes.contains(current) || current == null) {
+                continue;
+            }
+            if (condition.test(current.getValue())) {
+                return current;
+            }
+            numbersQueue.addAll(current.getChildren());
+            checkedNodes.add(current);
+
+        } while (current != null);
+        return null;
+    }
+}
