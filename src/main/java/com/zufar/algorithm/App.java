363aa634179201007422cbd46c64d29e70b4ee38
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2020-04-04 15:23:17 +0400

    Initial commit

---
+++ src/main/java/com/zufar/algorithm/App.java
@@ -0,0 +1,9 @@
+package com.zufar.algorithm;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Here will be algorithms." );
+    }
+}
