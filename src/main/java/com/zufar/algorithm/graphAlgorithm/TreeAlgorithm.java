f0f9f3e72e15e5e199a5c434db7fb1042fbaf1ee
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2020-04-05 16:13:49 +0400

    BinarySearch algorithm was added.

---
+++ src/main/java/com/zufar/algorithm/graphAlgorithm/TreeAlgorithm.java
@@ -0,0 +1,30 @@
+package com.zufar.algorithm.graphAlgorithm;
+
+public class TreeAlgorithm {
+
+    public static void binarySearch(int[] array, int first, int last, int item) {
+        int position;
+        int comparisonCount = 1;    // для подсчета количества сравнений
+
+        // для начала найдем индекс среднего элемента массива
+        position = (first + last) / 2;
+
+        while ((array[position] != item) && (first <= last)) {
+            comparisonCount++;
+            if (array[position] > item) {  // если число заданного для поиска
+                last = position - 1; // уменьшаем позицию на 1.
+            } else {
+                first = position + 1;    // иначе увеличиваем на 1
+            }
+            position = (first + last) / 2;
+        }
+        if (first <= last) {
+            System.out.println(item + " является " + ++position + " элементом в массиве");
+            System.out.println("Метод бинарного поиска нашел число после " + comparisonCount +
+                    " сравнений");
+        } else {
+            System.out.println("Элемент не найден в массиве. Метод бинарного поиска закончил работу после "
+                    + comparisonCount + " сравнений");
+        }
+    }
+}
