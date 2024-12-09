81e73021d4a3eacba1de26a5b7884d3f419d72de
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-12-09 01:49:09 +0000

    ContainsDuplicateWithSorting was added

---
+++ algorithms/main/java/arrays/easy/ContainsDuplicateWithSorting.java
@@ -0,0 +1,16 @@
+package arrays.easy;
+
+import java.util.Arrays;
+
+public class ContainsDuplicateWithSorting {
+
+    public boolean hasDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
